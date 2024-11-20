9bc464e1480b9fd85acc7cd9dd0c2950540dd934
kookie2332 <kookie2332@gmail.com>
2024-11-21 04:38:09 +1100

    baseline hooks implemented

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,15 @@
 
 #include <SokuLib.hpp>
 #include <libloaderapi.h>
+#include <string>
 
 static bool init = false;
 static bool frame_changed = true;
+static int ping = -1;
+static int player_delay = -1;
+static int opponent_delay = -1;
+static int current_rollback = -1;
+static uint8_t max_rollback = 0;
 
 static int (SokuLib::BattleClient::* ogBattleClientOnProcess)();
 static int (SokuLib::BattleClient::* ogBattleClientOnRender)();
@@ -32,8 +38,23 @@ static SokuLib::SWRFont font;
 static HMODULE giuroll;
 static bool desynced;
 
-typedef bool (__cdecl *desync_func)(void);
+typedef bool(__cdecl *desync_func)(void);
 desync_func is_likely_desynced;
+
+typedef int(__cdecl* ping_func)(void);
+ping_func get_ping;
+
+typedef int(__cdecl* player_delay_func)(void);
+player_delay_func get_player_delay;
+
+typedef int(__cdecl* opponent_delay_func)(void);
+opponent_delay_func get_opponent_delay;
+
+typedef int(__cdecl* current_rollback_func)(void);
+current_rollback_func get_current_rollback;
+
+typedef uint8_t(__cdecl* max_rollback_func)(void);
+max_rollback_func get_max_rollback;
 
 bool __fastcall giuroll_loaded() { return giuroll != NULL; }
 
@@ -42,6 +63,19 @@ bool __fastcall giuroll_loaded() { return giuroll != NULL; }
 * @return true if the function is not NULL, and false otherwise.
 */
 bool __fastcall desync_func_detected() { return is_likely_desynced != NULL; }
+
+/**
+* Function that checks if the get_ping function has been loaded or not.
+*/
+bool __fastcall ping_func_detected() { return get_ping != NULL; }
+
+bool __fastcall player_delay_func_detected() { return get_player_delay != NULL; }
+
+bool __fastcall opponent_delay_func_detected() { return get_opponent_delay != NULL; }
+
+bool __fastcall current_rollback_func_detected() { return get_current_rollback != NULL; }
+
+bool __fastcall max_rollback_func_detected() { return get_max_rollback != NULL; }
 
 void setText(const char* content) {
 	/*
@@ -128,8 +162,19 @@ void __fastcall Generic_OnProcess(void* This, char* init_message) {
 		OnLoad(This);
 		init = true;
 	}
-	if (desync_func_detected() && frame_changed) {
-		desynced = is_likely_desynced();
+	if (frame_changed) {
+		if (desync_func_detected()) desynced = is_likely_desynced();
+		if (ping_func_detected()) ping = get_ping();
+		if (player_delay_func_detected())player_delay = get_player_delay();
+		if (opponent_delay_func_detected()) opponent_delay = get_opponent_delay();
+		if (current_rollback_func_detected()) current_rollback = get_current_rollback();
+		if (max_rollback_func_detected()) {
+			max_rollback = get_max_rollback();
+			puts(("Max rollback: " + std::to_string(max_rollback)).c_str());
+		}
+
+		//int fps = *(int*)0x8985d8;
+		//puts(("fps: " + std::to_string(fps)).c_str());
 		frame_changed = false;
 	}
 }
@@ -147,19 +192,76 @@ int __fastcall CBattleClient_OnProcess(SokuLib::BattleClient* This) {
 bool __fastcall CNetworkMenu_OnProcess(SokuLib::MenuConnect* This) {
 	if (!network_init) {
 		giuroll = GetModuleHandleA("giuroll.dll");
-		if (!giuroll_loaded()) puts("WARNING: GIUROLL NOT DETECTED.");
+		bool gl = giuroll_loaded();
 
-		is_likely_desynced = (desync_func)GetProcAddress(
-			giuroll,
-			"is_likely_desynced");
-		if (giuroll_loaded() && !desync_func_detected()) {
-			puts("WARNING: DESYNC FUNCTION NOT DETECTED. MAKE SURE YOU ARE USING GIUROLL VERSION 0.6.13 OR LATER.");
-			int msgboxID = MessageBox(
-				NULL,
-				(LPCTSTR)"Desync function not detected.\nMake sure you are using Giuroll version 0.6.13 or later.",
-				(LPCTSTR)"Warning",
-				MB_OK | MB_ICONWARNING
-			);
+		if (!gl) puts("WARNING: GIUROLL NOT DETECTED.");
+
+		is_likely_desynced = (desync_func)GetProcAddress(giuroll, "is_likely_desynced");
+		get_ping = (ping_func)GetProcAddress(giuroll, "get_ping");
+		get_player_delay = (player_delay_func)GetProcAddress(giuroll, "get_player_delay");
+		get_opponent_delay = (opponent_delay_func)GetProcAddress(giuroll, "get_opponent_delay");
+		get_current_rollback = (current_rollback_func)GetProcAddress(giuroll, "get_current_rollback");
+		get_max_rollback = (max_rollback_func)GetProcAddress(giuroll, "get_max_rollback");
+
+		if (gl) {
+			bool warnings_present = false;
+			std::string warning_message = "";
+			if (!desync_func_detected()) {
+				puts("WARNING: DESYNC FUNCTION NOT DETECTED. MAKE SURE YOU ARE USING GIUROLL VERSION 0.6.13 OR LATER.");
+				warning_message += "Desync function not detected (Giuroll >= 0.6.13).\n";
+			}
+			else {
+				puts("Hooked desync func.");
+			}
+
+			if (!ping_func_detected()) {
+				puts("WARNING: PING FUNCTION NOT DETECTED. MAKE SURE YOU ARE USING GIUROLL VERSION 0.6.18 OR LATER.");
+				warning_message += "Ping function not detected (Giuroll >= 0.6.18).\n";
+			}
+			else {
+				puts("Hooked ping func.");
+			}
+
+			if (!player_delay_func_detected()) {
+				puts("WARNING: PLAYER DELAY FUNCTION NOT DETECTED. MAKE SURE YOU ARE USING GIUROLL VERSION 0.6.18 OR LATER.");
+				warning_message += "Player delay function not detected (Giuroll >= 0.6.18).\n";
+			}
+			else {
+				puts("Hooked player delay func.");
+			}
+
+			if (!opponent_delay_func_detected()) {
+				puts("WARNING: OPPONENT DELAY FUNCTION NOT DETECTED. MAKE SURE YOU ARE USING GIUROLL VERSION 0.6.18 OR LATER.");
+				warning_message += "Opponent delay function not detected (Giuroll >= 0.6.18).\n";
+			}
+			else {
+				puts("Hooked opponent delay func.");
+			}
+
+			if (!current_rollback_func_detected()) {
+				puts("WARNING: CURRENT ROLLBACK FUNCTION NOT DETECTED. MAKE SURE YOU ARE USING GIUROLL VERSION 0.6.18 OR LATER.");
+				warning_message += "Current rollback function not detected (Giuroll >= 0.6.18).\n";
+			}
+			else {
+				puts("Hooked current rollback func.");
+			}
+
+			if (!max_rollback_func_detected()) {
+				puts("WARNING: MAX ROLLBACK FUNCTION NOT DETECTED. MAKE SURE YOU ARE USING GIUROLL VERSION 0.6.18 OR LATER.");
+				warning_message += "Max rollback function not detected (Giuroll >= 0.6.18).\n";
+			}
+			else {
+				puts("Hooked max rollback func.");
+			}
+
+			if (!warning_message.empty()) {
+				int msgboxID = MessageBox(
+					NULL,
+					(LPCTSTR)warning_message.c_str(),
+					(LPCTSTR)"Warning",
+					MB_OK | MB_ICONWARNING
+				);
+			}
 		}
 		
 		network_init = true;
